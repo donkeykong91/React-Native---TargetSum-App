322d9b46aa18058eb98421c84ecdaee4c4987e05
Evelyn Contreras <evelyncontreras@Evelyns-MacBook-Air.local>
2018-10-08 11:41:07 -0700

    Added Game component to hold game logic

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Game from './Game'
+
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <Game />
+
+    );
+
+  }
+
+}
+
+
+export default App;
